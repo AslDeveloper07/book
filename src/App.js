6f52fdb584b4f8fbd6b73f5374010d7960488aaf
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-06 02:35:37 +0500

    fs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const API = [
+  {
+    id: 1,
+    image: "https://m.media-amazon.com/images/I/81TP4P8RDWL._SY466_.jpg",
+    author: " Robert Galbraith",
+    title: "The Fighting Bunch",
+  },
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/I/81sA41R62UL._SY466_.jpg",
+    author: " Rebecca Yarros",
+    title: "An Inside Job",
+  },
+  {
+    id: 3,
+    image: "https://m.media-amazon.com/images/I/51AGsQ7Q+VL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+  {
+    id: 4,
+    image: "https://m.media-amazon.com/images/I/51YEkiDk+aL._SY445_SX342_.jpg",
+    author: " Michelle Heard",
+    title: "Death (Mafia Empire Book 3",
+  },
+  {
+    id: 5,
+    image: "https://m.media-amazon.com/images/I/51pPUZJcj5S._SY445_SX342_.jpg",
+    author: " Robert Galbraith",
+    title: "The Fighting Bunch",
+  },
+  {
+    id: 6,
+    image: "https://m.media-amazon.com/images/I/91S6JMLcDfL._SY466_.jpg",
+    author: " Rebecca Yarros",
+    title: "An Inside Job",
+  },
+  {
+    id: 7,
+    image: "https://m.media-amazon.com/images/I/514eMlhuxUL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+  {
+    id: 8,
+    image: "https://m.media-amazon.com/images/I/71yfI1pxj+L._SY466_.jpg",
+    author: " Michelle Heard",
+    title: "Death (Mafia Empire Book 3",
+  },
+  {
+    id: 9,
+    image: "https://m.media-amazon.com/images/I/51ljJ3vklVL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+];
 
-// export default App;
+const BookList = ({author, image, id, title}) => {
 
+    return(
+        <section className="bookList">
+<img src={Image} alt={title} />
+<div className="text">
+
+<h2>{title}</h2>
+<h4>{author}</h4>
+</div>
+        </section>
+    )
+};
+
+const Book = () => {
+
+    return(
+        <article className="book container">
+
+{
+    API.map((book)=>{
+        <BookList
+        author={book.author}
+        title={book.title}
+        image={book.image}
+        key={book.id}
+        />
+    })
+}
+        </article>
+    )
+};
+
+const Main = () => {
+  return (
+    <>
+      <Book />
+    </>
+  );
+};
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(
+  <>
+    <Main />
+  </>
+);
