c88eb5e95f9ecdb0a36773b0f05f111d8d0aff53
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-05 18:07:46 +0500

    men projectni tugatdim

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const API = [
+  {
+    id: 1,
+    image: "https://m.media-amazon.com/images/I/81TP4P8RDWL._SY466_.jpg",
+    author: " Robert Galbraith",
+    title: "The Fighting Bunch",
+  },
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/I/81sA41R62UL._SY466_.jpg",
+    author: " Rebecca Yarros",
+    title: "An Inside Job",
+  },
+  {
+    id: 3,
+    image: "https://m.media-amazon.com/images/I/51AGsQ7Q+VL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+  {
+    id: 4,
+    image: "https://m.media-amazon.com/images/I/51YEkiDk+aL._SY445_SX342_.jpg",
+    author: " Michelle Heard",
+    title: "Death (Mafia Empire Book 3",
+  },
+  {
+    id: 5,
+    image: "https://m.media-amazon.com/images/I/51pPUZJcj5S._SY445_SX342_.jpg",
+    author: " Robert Galbraith",
+    title: "The Fighting Bunch",
+  },
+  {
+    id: 6,
+    image: "https://m.media-amazon.com/images/I/91S6JMLcDfL._SY466_.jpg",
+    author: " Rebecca Yarros",
+    title: "An Inside Job",
+  },
+  {
+    id: 7,
+    image: "https://m.media-amazon.com/images/I/514eMlhuxUL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+  {
+    id: 8,
+    image: "https://m.media-amazon.com/images/I/71yfI1pxj+L._SY466_.jpg",
+    author: " Michelle Heard",
+    title: "Death (Mafia Empire Book 3",
+  },
+  {
+    id: 9,
+    image: "https://m.media-amazon.com/images/I/51ljJ3vklVL._SY445_SX342_.jpg",
+    author: " Karla Sorensen",
+    title: "Troubled Blood",
+  },
+];
+
+const BookList = ({ author, title, image }) => {
+  return (
+    <section className="bookList">
+      <img src={image} alt={title} />
+      <div className="text">
+        <h2>{title}</h2>
+        <h4>{author}</h4>
+      </div>
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book container">
+      {API.map((book) => (
+        <BookList
+          author={book.author}
+          title={book.title}
+          image={book.image}
+          key={book.id}
+        />
+      ))}
+    </article>
+  );
+};
+
+const Main = () => {
+  return (
+    <>
+      <Book />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
